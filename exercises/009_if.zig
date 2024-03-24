440c06dd9c1c784597f9896ed3c86eb188f5c1bb
Jason Hilder <jhilder95@gmail.com>
2024-03-24 22:10:30 +0200

    Initial commit with current progress.

--- exercises/009_if.zig
+++ exercises/009_if.zig
@@ -19,15 +19,16 @@
 // to true and false.
 //
 const std = @import("std");
+const print = std.debug.print;
 
 pub fn main() void {
     const foo = 1;
 
     // Please fix this condition:
-    if (foo) {
+    if (foo == 1) {
         // We want our program to print this message!
-        std.debug.print("Foo is 1!\n", .{});
+        print("Foo is 1!\n", .{});
     } else {
-        std.debug.print("Foo is not 1!\n", .{});
+        print("Foo is not 1!\n", .{});
     }
 }
